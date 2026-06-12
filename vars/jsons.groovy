e07d441020df8a7b126d6070da1d1363eb3f5998
StasX <s.mestechkin@gmail.com>
2026-06-12 19:25:56 +0300

    Fix typo

--- vars/jsons.groovy
+++ vars/jsons.groovy
@@ -30,5 +30,5 @@ def saveToJson(Map data, String fileName){
         ],
         description: data['description']
     ]
-    return writeJSON(file: fileName, json: payload, pretty: 4)
+    writeJSON(file: fileName, json: payload, pretty: 4)
 }
