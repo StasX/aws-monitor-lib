3261ee8303f9a735842ed3c6dbf258c8eb7b9f8f
StasX <s.mestechkin@gmail.com>
2026-06-13 17:26:52 +0300

    Add configuration update

---
+++ vars/config.groovy
@@ -0,0 +1,21 @@
+def update(String repoOwner, String repo, String email, String version){
+    withCredentials([usernamePassword(credentialsId: 'github_creds', 
+    usernameVariable: 'USER', 
+    passwordVariable: 'TOKEN')]) {
+        withEnv([
+            "REPO_OWNER=${repoOwner}",
+            "REPO=${repo}",
+            "EMAIL=${email}",
+            "VERSION=${version}"
+        ]) {
+            sh '''
+                git config user.name "$USER"
+                git config user.email "$EMAIL"
+                git add .app-info.json
+                git commit -m "Update next app version: $VERSION in .app-info.json"
+                git remote set-url origin https://x-access-token:$TOKEN@github.com/$REPO_OWNER/$REPO.git
+                git push origin main
+            '''
+        }
+    }
+}
