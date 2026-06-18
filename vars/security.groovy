a153bce47dcd42b98f4b283f2e301e8e6c334c43
StasX <s.mestechkin@gmail.com>
2026-06-18 18:47:33 +0300

    Add tools installations

--- vars/security.groovy
+++ vars/security.groovy
@@ -1,20 +1,13 @@
 def banditScan (){
-    sh """
-    pip install --user bandit
-
-    python -m bandit -r ./ -x ./.venv,./venv
-    """
+    sh "python -m bandit -r ./ -x ./.venv,./venv"
 }
 
-def checkovScan(String path, String pathFlag, String framework, String venv = "/tmp/.venv") {
-    sh "${venv}/bin/checkov ${pathFlag} ${path} --framework ${framework}"
+def checkovScan(String path, String pathFlag, String framework, String venv = ".venv") {
+    sh "${venv}/bin/python -m checkov.main ${pathFlag} ${path} --framework ${framework}"
 }
 
 def semgrepScan(){
     sh """
-    python3 -m venv .venv
-    . .venv/bin/activate
-    .venv/bin/python -m pip install semgrep
     .venv/bin/semgrep scan \
     --config=p/python \
     --config=p/dockerfile \
